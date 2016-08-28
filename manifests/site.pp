2181f38638f480e746ae1394289bfe14c050012d
srinath77 <srinath.degala@gmail.com>
2016-08-28 11:41:54 -0500

    deleted araving module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,3 @@
-node "node.example.com","node2.example.com" {
-include 'apache','git','webserver','user','aravind'
+node "node.example.com","node2.example.com","node3.example.com"{
+include 'apache','git','webserver','user'
 } 
